f66970c22de5244b82947cf7e9d938b6a5d37c9a
yosef-schmidt <yosefsemconta@gmail.com>
2023-09-19 18:43:13 -0300

    Amanhã faço a conexão no processar com os outros d dados a serem inseridos

--- index.php
+++ index.php
@@ -1,15 +1,32 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Churras</title>
 </head>
 <body>
+    <h1>Churrasco do Yosef Schmidt Abreu de Medeiros CPET</h1>
     <form action="processar_formulario.php" method="post">
-        <label for="Nome">Nome:</label>
+        <p>Coloque seu nome abaixo:</p>
+        <label for="nome">Nome:</label>
         <input type="text" id="nome" name="nome" required>
+        
+        <p>A carne custa 10 R$, pão com alho 3 R$, qual dos dois você quer ?</p>
+        <label>
+            <input type="radio" name="opcao" value="carne" required> Carne (10 R$)
+        </label>
+        <label>
+            <input type="radio" name="opcao" value="pao_com_alho" required> Pão com alho (3 R$)
+        </label>
+        
+        <p>Quantidade desejada:</p>
+        <label for="quantidade">Quantidade:</label>
+        <input type="number" id="quantidade" name="quantidade" required>
+        
         <input type="submit" value="Enviar">
     </form>
 </body>
+</html>
+
 </html>
