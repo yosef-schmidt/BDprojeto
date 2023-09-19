cb2f84837bb50cb214b1985d42890f4d974a9d32
yosef-schmidt <yosefsemconta@gmail.com>
2023-09-19 17:47:36 -0300

    deu certo

--- processar_formulario.php
+++ processar_formulario.php
@@ -1,23 +1,23 @@
 <?php
-// Conexão com o banco de dados
-$host = "localhost"; // Endereço do servidor MySQL
-$usuario = "root"; // Nome de usuário do MySQL
-$senha = "nova_senha"; // Senha do MySQL
-$banco = "churrasco"; // Nome do banco de dados
 
+$host = "localhost"; 
+$usuario = "yosef"; 
+$senha = "sua_senha"; 
+$banco = "churrasco"; 
+echo "deu certo";
 $conn = new mysqli($host, $usuario, $senha, $banco);
 
-// Verifique a conexão
+
 if ($conn->connect_error) {
     die("Erro de conexão: " . $conn->connect_error);
 }
 
-// Verifique se o formulário foi enviado
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Recupere o nome do formulário
+    
     $nome = $_POST['nome'];
 
-    // SQL para inserir o nome na tabela
+    
     $sql = "INSERT INTO convidados (nome) VALUES ('$nome')";
 
     if ($conn->query($sql) === TRUE) {
@@ -27,6 +27,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 }
 
-// Feche a conexão com o banco de dados
+
 $conn->close();
 ?>
