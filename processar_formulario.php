e350fb7ae2ea2094ca2858195c5232b03a7a982a
yosef-schmidt <yosefsemconta@gmail.com>
2023-09-20 10:40:58 -0300

    consulta e calculo do valor total do churrasco.

--- processar_formulario.php
+++ processar_formulario.php
@@ -37,23 +37,30 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 
 }
-$sql = "SELECT quantidade, tipo_de_comida FROM comida";
-
-$resultado = $conn->query($sql);
-
+$sql3 = "SELECT quantidade, tipo_de_comida, c.nome FROM comida AS cm
+        INNER JOIN convidados AS c ON cm.convidado_id = c.id";
+$resultado = $conn->query($sql3);
+$totalcarne=0;
+$totalpao=0;
 if ($resultado->num_rows > 0) {
     // Loop através dos resultados
     while ($linha = $resultado->fetch_assoc()) {
         $quantidade = $linha["quantidade"];
         $tipo_de_comida = $linha["tipo_de_comida"];
-        
-        // Faça algo com a quantidade e o tipo de comida, por exemplo, exiba na tela
-        echo "Quantidade: " . $quantidade . ", Tipo de Comida: " . $tipo_de_comida . "<br>";
+        $nomeConvidado = $linha["nome"];
+        echo "O convidado $nomeConvidado pediu $quantidade de $tipo_de_comida <br>";
+        if($tipo_de_comida=="carne"){
+            $totalcarne=$totalcarne+1;
+        }
+        if($tipo_de_comida=="pao_com_alho"){
+            $totalpao=$totalpao+1;    
+        }   
     }
+    $valortol=$totalpao*3+$totalcarne*10;
+    echo "Vai custar no total o churrasco $valortol de reais"; 
 } else {
-    echo "Nenhum registro encontrado na tabela 'comida'.";
+    echo "Nenhum registro encontrado na tabela 'comida'";
 }
 
 $conn->close();
 ?>
-
