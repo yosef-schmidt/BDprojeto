401a3102a629895ad8623869e31fa543237b33ea
yosef-schmidt <yosefsemconta@gmail.com>
2023-09-20 09:26:05 -0300

    testando novo forms

--- processar_formulario.php
+++ processar_formulario.php
@@ -4,29 +4,37 @@ $host = "localhost";
 $usuario = "yosef"; 
 $senha = "sua_senha"; 
 $banco = "churrasco"; 
-echo "deu certo";
-$conn = new mysqli($host, $usuario, $senha, $banco);
 
+$conn = new mysqli($host, $usuario, $senha, $banco);
 
 if ($conn->connect_error) {
     die("Erro de conexão: " . $conn->connect_error);
 }
 
-
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    
     $nome = $_POST['nome'];
+    $opcao = $_POST['opcao'];
+    $quantidade = $_POST['quantidade'];
 
-    
-    $sql = "INSERT INTO convidados (nome) VALUES ('$nome')";
+    // Inserir nome do convidado na tabela "convidados"
+    $sql1 = "INSERT INTO convidados (nome) VALUES ('$nome')";
 
-    if ($conn->query($sql) === TRUE) {
+    if ($conn->query($sql1) === TRUE) {
         echo "Nome inserido com sucesso!";
     } else {
         echo "Erro ao inserir o nome: " . $conn->error;
     }
-}
 
+    // Inserir escolha de comida e quantidade na tabela "comida"
+    $sql2 = "INSERT INTO comida (quantidade, tipo_de_comida, convidado_id) VALUES ($quantidade, '$opcao', LAST_INSERT_ID())";
+
+    if ($conn->query($sql2) === TRUE) {
+        echo "Escolha de comida inserida com sucesso!";
+    } else {
+        echo "Erro ao inserir a escolha de comida: " . $conn->error;
+    }
+}
 
 $conn->close();
 ?>
+
